fix settings so training happens in mixed precision

setup_model_for_training.py
@@ -44,9 +44,11 @@ def wrap_fsdp2(model: torch.nn.Module) -> torch.nn.Module:
 
     # 4) Mixed-precision policy (bf16)
     mp_policy = MixedPrecisionPolicy(
+        # computations happen in bf16
         param_dtype=torch.bfloat16, 
-        reduce_dtype=torch.bfloat16,
-        output_dtype=torch.bfloat16)
+        # reduce operations should all happen in fp32 to preserve precision
+        reduce_dtype=torch.float32,
+    )
 
     # 4) FSDP2 wrap each block
     for idx, block in enumerate(layers):
@@ -104,7 +106,6 @@ def setup_model(
 ) -> torch.nn.Module | SVDModel:
     base_model_args = {
         "pretrained_model_name_or_path": kwargs['model_name_or_path'],
-        "torch_dtype": torch.bfloat16,
     }
     base_model_args["attn_implementation"] = "flash_attention_2"
 